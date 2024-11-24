6c5275f9a13e9648f715fbd869184bf3065cd3d5
Alex Sobakar <xaosaki@gmail.com>
2024-11-24 14:57:54 -0800

    feat: added fetching order details from DB

--- src/app/order-details/[orderId]/page.tsx
+++ src/app/order-details/[orderId]/page.tsx
@@ -1,18 +1,58 @@
 import React from "react";
 import { redirect } from "next/navigation";
+import { prisma } from "@/lib/prisma";
+import { dateFormatter } from "@/lib/dateFormatter";
 
 export default async function Page({ params }: { params: Promise<{ orderId: string }> }) {
   const { orderId } = await params;
 
-  if (!orderId) {
+  const order = await prisma.order.findFirst({
+    where: {
+      orderId
+    }
+  });
+
+  if (!order) {
     redirect("/404");
   }
 
   return (
     <>
       <section className="container px-4 mx-auto">
-        <h2>Order Details</h2>
-        <p className="leading-6">OrderId: {orderId}</p>
+        <h2 className="text-2xl">Order Details</h2>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Field</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Order ID</td>
+              <td>{order.orderId}</td>
+            </tr>
+            <tr>
+              <td>User name</td>
+              <td>{order.username}</td>
+            </tr>
+            <tr>
+              <td>Quantity</td>
+              <td>{order.quantity}</td>
+            </tr>
+            <tr>
+              <td>Address</td>
+              <td>
+                {order.country}, {order.region}, {order.city}
+              </td>
+            </tr>
+            <tr>
+              <td>Created at</td>
+              <td>{dateFormatter(order.createdAt)}</td>
+            </tr>
+          </tbody>
+        </table>
       </section>
     </>
   );
