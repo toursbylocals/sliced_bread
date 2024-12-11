1eaf0cf0b03b0dbd58675aa2baf2b04de0c6e95e
doug schlenker <dougschlenker@gmail.com>
2024-12-11 09:38:20 -0500

    update package depenedencies and gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -30,7 +30,7 @@ export default function Home() {
         text-sm text-center sm:text-left"
         >
           <li className="mb-2">
-            Get started by editing{' '}
+            Get started by editing{" "}
             <code
               className="bg-black/[.05] 
             dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold"
@@ -42,26 +42,6 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border 
-            border-solid border-transparent transition-colors 
-            flex items-center justify-center bg-foreground 
-            text-background gap-2 hover:bg-[#383838] 
-            dark:hover:bg-[#ccc] text-sm 
-            sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
           <a
             className="rounded-full border border-solid 
             border-black/[.08] dark:border-white/[.145] 
@@ -89,7 +69,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/file-text.svg" alt="File icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/file-text.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
           Learn
         </a>
         <a
@@ -99,7 +85,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
           Examples
         </a>
         <a
@@ -109,10 +101,16 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
           Go to nextjs.org →
         </a>
       </footer>
     </div>
-  )
+  );
 }
